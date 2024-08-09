a6316e60e0070f14c166904e7401af3f3581de79
Diego Rodriguez <diego.rodriguez@avivel.com.pe>
2024-08-09 02:13:14 -0500

    subiendo cambios en docerfile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  HOST: 'http://192.168.2.30:9002/aviventas-api/api',
+  //HOST: 'http://192.168.2.30:9002/aviventas-api/api',
+  HOST: 'http://51.222.142.208:8686/aviventas-api/api',
   REINTENTOS: 3,
   OAUTH_RESPONSE: 'access_token',
   ZONA: 'Lurín'
